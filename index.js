9aef242c3f1420067b25a9d161b97c7f1bd88eb0
gameeblock144 <160583601+gameeblock144@users.noreply.github.com>
2024-03-15 17:45:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const mySecret = process.env['token']
+// to raise the level of robot accounts | رفع لفل حسابات بروبوت
+const Discord = require("discord.js-selfbot-v13"); 
+const client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS]
+});
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+const {userAccount} = require("sphinx-run");
+new userAccount(client, Discord).leveling({
+    channel: 'prof',
+    randomLetters: false, 
+    time: 120000, //الوقت 
+    type: 'ar' //الغةا 
+  
+});
+client.login(process.env.token);
+
+const express = require("express");
+const app = express();
+app.listen(() => console.log("Server started"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
